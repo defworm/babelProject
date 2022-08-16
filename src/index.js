7df05c2adfa678cd763fe404cf215c1e058c3624
Crystal Korth <crystalkorth@gmail.com>
2022-08-15 19:23:04 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const multiply = (a,b) => a * b
+const firstNum = 2
+const secondNum = 3
+const thirdNum =4
+let product = multiply(firstNum, secondNum, thirdNum)
+console.log(`The product of ${firstNum} and ${secondNum} and ${thirdNum} is ${product}`)
